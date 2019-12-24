156e0ffe6d917313d922f204abc30840db2842f7
jhow <jhonatta@cotacoesecompras.com>
2019-12-24 00:43:50 -0300

    Add has max len

--- src/Validations/StringValidationContract.php
+++ src/Validations/StringValidationContract.php
@@ -20,4 +20,22 @@ trait StringValidationContract
 
         return $this;
     }
+
+    public function hasMinLen($val, $min, $property, $message)
+    {
+        if((empty(trim($val)) || is_null($val)) || strlen(trim($val)) < $min){
+            $this->addNotification($property, $message);
+        }
+        return $this;
+    }
+
+    public function hasMaxLen($val, $max, $property, $message)
+    {
+        if(strlen(trim($val)) > $max){
+            $this->addNotification($property, $message);
+        }
+        return $this;
+    }
+
+    
 }
--- tests/StringValidationContractTests.php
+++ tests/StringValidationContractTests.php
@@ -23,11 +23,88 @@ class StringValidationContractTests extends TestCase
 
     public function testIsNullOrEmpty()
     {
-        $wrong = (new Contract())
+        $right = (new Contract())
                     ->requires()
                     ->isNullOrEmpty(null, "string", "String is Null")
                     ->isNullOrEmpty("", "string", "String is Empty");
-        //$this->assertTrue($wrong->valid());
-        $this->assertEquals(0, count($wrong->notifications()));
+        $this->assertTrue($right->valid());
+        $this->assertEquals(0, count($right->notifications()));
+
+        $wrong = (new Contract())
+                    ->requires()
+                    ->isNullOrEmpty("Some valid string", "string", "String is Null");
+        $this->assertTrue($wrong->invalid());
     }
+
+    public function testHasMinLen()
+    {
+        $wrong = (new Contract())
+                    ->requires()
+                    ->hasMinLen("null", 5, "string", "String len is less than permited");
+        $this->assertEquals(false, $wrong->valid());
+        $this->assertEquals(1, count($wrong->notifications()));
+
+        $right = (new Contract())
+                ->requires()
+                ->hasMinLen("Some Valid String", 5, "string", "String len is less than permited");
+
+        $this->assertEquals(true,$right->valid());
+    }
+
+    
+    public function testHasMaxLen()
+    {
+        $wrong = (new Contract())
+                    ->requires()
+                    ->hasMaxLen("null", 3, "string", "String len is more than permited");
+        $this->assertEquals(false, $wrong->valid());
+        $this->assertEquals(1, count($wrong->notifications()));
+
+        $right = (new Contract())
+                ->requires()
+                ->hasMaxLen("Some", 5, "string", "String len is less than permited");
+
+        $this->assertEquals(true, $right->valid());
+    }
+
+    /*
+    public function testHasLen()
+    {
+
+    }
+
+    public function testAreEquals()
+    {
+
+    }
+
+    public function testAreNotEquals()
+    {
+
+    }
+
+    public function testIsEmail()
+    {
+
+    }
+
+    public function testIsEmailOrEmpty()
+    {
+
+    }
+
+    public function testIsUrl()
+    {
+
+    }
+
+    public function testIsUrlOrEmpty()
+    {
+
+    }
+
+    public function testIsDigit()
+    {
+
+    }*/
 }
